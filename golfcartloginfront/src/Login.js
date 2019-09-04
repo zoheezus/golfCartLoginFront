ab4c26d5ea804569d4743fbf002cc3bd6bf306dd
zoheezus <zohaib0126@gmail.com>
2019-09-03 23:03:01 -0400

    remove

---
+++ golfcartloginfront/src/Login.js
@@ -0,0 +1,14 @@
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+
+class Login extends Component {
+    constructor(props){
+        super(props);
+    this.state={
+            username:'',
+            password:''
+        }
+    }
+}
